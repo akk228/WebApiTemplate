1248186598e665a756c65340af2d12370816f4c3
akk228 <andreykpq@gmail.com>
2023-01-30 13:08:28 +0100

    Add project files.

---
+++ Controllers/PingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApiTemplate.Controllers
+{
+    public class PingController : ApiController
+    {
+        [Route("api/ping")]
+        public string Get()
+        {
+            return "Ping Succesfull!";
+        }
+    }
+}
